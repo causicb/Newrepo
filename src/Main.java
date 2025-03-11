187599375540e2629f0ed5c8b378420ad6d0a92b
causicb <benjo1306@gmail.com>
2025-03-11 18:29:26 +0100

    Refactoring #2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
         int sum = 0;
-        for (int i = 0; i < 100; i++){
+        for (int i = 0; i < 100; i++) {
 
-            if (i % 3 == 0){
+            if (i % 3 == 0) {
                 sum = sum + i;
             }
         }
