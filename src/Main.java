6132ed2964394ae21b449608edcb625e2244f705
causicb <benjo1306@gmail.com>
2025-03-11 18:26:09 +0100

    Brojevi djeljivi sa 3

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ public class Main {
         int sum = 0;
         for (int i = 0; i < 100; i++){
 
-            if (i % 2 == 0){
+            if (i % 3 == 0){
                 sum = sum + i;
             }
         }
